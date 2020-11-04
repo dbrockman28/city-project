d928be3d36b46a37a37e24240985e53541c203c9
Rowdy Goat <dbrockman28@gmail.com>
2020-11-04 16:05:34 -0600

    testing get from utelly

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+let getStream = function(input) {
+  fetch("https://rapidapi.p.rapidapi.com/lookup?term=" + input + "&country=us", {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-key": "786577e5f2msh5a37d5421a86c8fp1cde07jsn46e335d06b99",
+      "x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com"
+    }
+  }).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    } else {
+      alert("Error: " + response.statusText);
+    }
+  })
+  .catch(function(error) {
+    alert("Unable to connect to Utelly")
+  })
+};
+
+getStream("breaking bad");
